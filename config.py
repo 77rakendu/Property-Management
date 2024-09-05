6d4054b86be8110b34335c8be2aea548ced6eda1
rakendu31 <rakenduid@gmail.com>
2024-09-05 13:57:50 -0400

    configured

--- config.py
+++ config.py
@@ -2,6 +2,6 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'b299860c3dc7b3d9a319e97a68cd0c2c')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
