275160f29ac38bddbbb0e13da650ec21358b070f
rakendu31 <rakenduid@gmail.com>
2024-09-05 17:06:59 -0400

    add and test REST APIs

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,12 @@
-# app/__init__.py
+# __init__.py
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_restful import Api
 from config import Config
 
 db = SQLAlchemy()
 migrate = Migrate()
-api = Api()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -15,20 +14,8 @@ def create_app(config_class=Config):
 
     db.init_app(app)
     migrate.init_app(app, db)
-    api.init_app(app)
 
-    from app.resources.property import PropertyResource, PropertyListResource
-    from app.resources.user import UserResource, UserListResource
-    from app.resources.booking import BookingResource, BookingListResource
-    from app.resources.payment import PaymentResource, PaymentListResource
-
-    api.add_resource(PropertyListResource, '/properties')
-    api.add_resource(PropertyResource, '/properties/<int:id>')
-    api.add_resource(UserListResource, '/users')
-    api.add_resource(UserResource, '/users/<int:id>')
-    api.add_resource(BookingListResource, '/bookings')
-    api.add_resource(BookingResource, '/bookings/<int:id>')
-    api.add_resource(PaymentListResource, '/payments')
-    api.add_resource(PaymentResource, '/payments/<int:id>')
+    from app.routes import init_app  # Import your routes initialization
+    init_app(app)  # Register routes
 
     return app
---
+++ app/resources/__init__.py
@@ -0,0 +1,4 @@
+# This file can be used to import all resource classes, if needed
+from .user import UserResource, UserListResource
+from .booking import BookingResource, BookingListResource
+from .property import PropertyResource, PropertyListResource
